060795eaf78ed1f47dce65013fcc92f256273408
Santosh Pandey <spandey@zscaler.com>
2020-03-17 15:29:19 +0530

    add docker files and documentation

---
+++ docker-compose/schema.sql
@@ -0,0 +1,24 @@
+create database userservice;
+
+\connect userservice;
+
+create table user_detail(
+	user_id serial PRIMARY KEY,
+	first_name varchar(100),
+	last_name varchar(100)
+);
+
+create table email(
+	id serial PRIMARY KEY,
+	user_id integer references user_detail(user_id),
+	mail varchar(100)
+);
+
+create table phone_number(
+	id serial PRIMARY KEY,
+	user_id integer references user_detail(user_id),
+	number varchar(20)
+);
+
+
+
